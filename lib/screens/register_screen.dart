0fa93808943b59be3f77b41b2e48fc3cc294d5e6
AbdallahMabrouck <mabrouck32@gmail.com>
2023-09-10 21:37:43 +0300

    modified register screen

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:kiranjaapp_driver/screens/login_screen.dart';
-import 'package:provider/provider.dart';
-import '../provider/auth_provider.dart';
 import '../widgets/image_picker.dart';
 import '../widgets/register_form.dart';
 
@@ -12,44 +9,17 @@ class RegisterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final _authData = Provider.of<AuthProvider>(context);
-    return SafeArea(
+    return const SafeArea(
       child: Scaffold(
         body: Center(
           child: Padding(
-            padding: const EdgeInsets.all(20.0),
+            padding: EdgeInsets.all(20.0),
             child: SingleChildScrollView(
               scrollDirection: Axis.vertical,
               child: Column(
                 children: [
-                  const ShopPicCard(),
-                  const RegisterForm(),
-                  _authData.loading
-                      ? Container()
-                      : Row(
-                          children: [
-                            ElevatedButton(
-                              onPressed: () {
-                                Navigator.pushNamed(context, LoginScreen.id);
-                              },
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: RichText(
-                                  text: const TextSpan(text: "", children: [
-                                    TextSpan(
-                                        text: "Already have an account ? ",
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold)),
-                                    TextSpan(
-                                        text: "Login",
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold))
-                                  ]),
-                                ),
-                              ),
-                            ),
-                          ],
-                        )
+                  ShopPicCard(),
+                  RegisterForm(),
                 ],
               ),
             ),
