df24281bdb01fb29a7bba3003c1d49daa040c94b
AbdallahMabrouck <mabrouck32@gmail.com>
2023-09-10 17:36:43 +0300

    created splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,14 +1,14 @@
 import 'dart:async';
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
+import 'package:get_storage/get_storage.dart';
 import 'home_screen.dart';
 import 'login_screen.dart';
 
-
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+  const SplashScreen({Key? key}) : super(key: key);
+
   static const String id = "splash-screen";
 
   @override
@@ -20,24 +20,16 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   void initState() {
+    super.initState();
     Timer(const Duration(seconds: 3), () {
-
-      FirebaseAuth.instance.authStateChanges().listen((User user){
-        if(user == null){
+      FirebaseAuth.instance.authStateChanges().listen((User? user) {
+        if (user == null) {
           Navigator.pushReplacementNamed(context, LoginScreen.id);
         } else {
-           Navigator.pushReplacementNamed(context, HomeScreen.id);
+          Navigator.pushReplacementNamed(context, HomeScreen.id);
         }
       });
-      // bool? boarding = store.read("onBoarding");
-      // boarding == null
-          // ? Navigator.pushReplacementNamed(context, WelcomeScreen.id)
-          // : boarding == true
-              // ? Navigator.pushReplacementNamed(context, MainScreen.id)
-              // : Navigator.pushReplacementNamed(context, WelcomeScreen.id);
-      // Navigator.pushNamed(context, WelcomeScreen.id);
     });
-    super.initState();
   }
 
   @override
@@ -58,7 +50,7 @@ class _SplashScreenState extends State<SplashScreen> {
               height: 20,
             ),
             const CircularProgressIndicator(
-              valueColor: AlwaysStoppedAnimation(Colors.white),
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
             )
           ],
         ),
