5631dc745fb042db0e5191bfd93bf32e64b081db
AbdallahMabrouck <mabrouck32@gmail.com>
2023-09-10 22:05:55 +0300

    created home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,6 @@
+import 'package:chips_choice/chips_choice.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:kiranjaapp_driver/services/firebase_services.dart';
 import 'package:kiranjaapp_driver/widgets/order_summary_card.dart';
@@ -11,88 +14,97 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final User? _user = FirebaseAuth.instance.currentUser;
+  final FirebaseServices _services = FirebaseServices();
+  String status = "";
 
-   User _user = FirebaseAuth.instance.currentUser;
-    FirebaseServices _services = FirebaseServices();
-    String status;
-
-     int tag = 0;
+  int tag = 0;
   List<String> options = [
-    "All",     
-    "Accepted", 
-    "Picked Up", 
-    "On the Way", 
+    "All",
+    "Accepted",
+    "Picked Up",
+    "On the Way",
     "Delivered"
   ];
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor.withOpacity(.2),
-      appBar: AppBar(title: const Text("Orders", 
-      style: TextStyle(color: Colors.white),), centerTitle: true,),
+      appBar: AppBar(
+        title: const Text(
+          "Orders",
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
       body: Column(
         children: [
-            SizedBox(
-                        height: 56, 
-                        width: MediaQuery.of(context).size.width,
-                        child: ChipsChoice<int>.single(
-                          choiceStyle: C2ChoiceStyle(borderRadius: const BorderRadius.all(Radius.circular(3),),),
-                          value: tag, 
-                          onChanged: (val){
-                            if(val == 0){
-                              setState(() {
-                                status = null;
-                              });
-                            }
-                            setState((){
-                            tag = val;
-                            if(val == 0){
-                              status = options[val];
-                            }
-                          });
-                          },
-                          choiceItem: C2Choice.listFrom<int, String>(
-                            source: options, 
-                            value: (i, v) => i, 
-                            label: (i, v) => v,
-                          ),
-                        ),
-                      ),
-
+          SizedBox(
+            height: 56,
+            width: MediaQuery.of(context).size.width,
+            child: ChipsChoice<int>.single(
+              choiceStyle: const C2ChipStyle(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(3),
+                ),
+              ),
+              value: tag,
+              onChanged: (val) {
+                if (val == 0) {
+                  setState(() {
+                    status = status;
+                  });
+                }
+                setState(() {
+                  tag = val;
+                  if (val == 0) {
+                    status = options[val];
+                  }
+                });
+              },
+              choiceItems: C2Choice.listFrom<int, String>(
+                source: options,
+                value: (i, v) => i,
+                label: (i, v) => v,
+              ),
+            ),
+          ),
           Container(
             child: StreamBuilder<QuerySnapshot>(
-            stream: _services.orders
-            .where("deliveryBoy.email", isEqualTo: _user.email)
-            .where("orderStatus", isEqualTo: tag == 0 ? null : status )
-            .snapshots(), 
-            builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-              if(snapshot.hasError){
-                return const Text("Something went wrong");
-              }  
- 
-          
-              if(snapshot.connectionState == ConnectionState.waiting){
-                return const Center(child: CircularProgressIndicator(),);
-              }     
+              stream: _services.orders
+                  .where("deliveryBoy.email", isEqualTo: _user!.email)
+                  .where("orderStatus", isEqualTo: tag == 0 ? null : status)
+                  .snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  return const Text("Something went wrong");
+                }
 
-              if(snapshot.data.size == 0){
-              return Center(child: Text("No $status orders"));
-              } 
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
 
-          
-              return Expanded(
-                child: ListView(
-                  children: snapshot.data.docs.map((DocumentSnapshot document){
-                    return Padding(
-                      padding: const EdgeInsets.only(left: 8, right: 8, bottom: 8),
-                      child: OrderSummaryCard(document: document),
-                    );
-                  }).toList(),
-                ),
-              );
-            },
+                if (snapshot.data!.size == 0) {
+                  return Center(child: Text("No $status orders"));
+                }
+
+                return Expanded(
+                  child: ListView(
+                    children:
+                        snapshot.data!.docs.map((DocumentSnapshot document) {
+                      return Padding(
+                        padding:
+                            const EdgeInsets.only(left: 8, right: 8, bottom: 8),
+                        child: OrderSummaryCard(document: document),
+                      );
+                    }).toList(),
+                  ),
+                );
+              },
             ),
           ),
         ],
